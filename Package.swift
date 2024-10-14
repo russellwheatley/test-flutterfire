323bddcc344d7b85bb6e058f03b886abc2dce6e9
russellwheatley <russellwheatley85@gmail.com>
2024-10-14 15:06:31 +0100

    update to test

--- Package.swift
+++ Package.swift
@@ -8,25 +8,29 @@
 import Foundation
 import PackageDescription
 
+// Using this as a wrapper around firebase core, this allows retrieval of it via remote package
+// whilst also preserving firebase core's Package.swift file needed by Flutter
 let package = Package(
   name: "remote_firebase_core",
   platforms: [
     .iOS("13.0"),
   ],
   products: [
-    .library(name: "remote-firebase-core", targets: ["remote_firebase_core"]),
-  ],
-  dependencies: [
-    // Add a local package dependency to firebase_core
-    .package(path: "packages/firebase_core/firebase_core/ios/firebase_core"),
+    .library(name: "firebase-core-target", targets: ["firebase_core_target"]),
   ],
   targets: [
     .target(
-      name: "remote_firebase_core",
-      dependencies: [
-        // Add the firebase_core package as a dependency
-        .product(name: "firebase-core", package: "firebase_core"),
-      ]
+      name: "firebase_core_target",
+      dependencies:[
+        .target(name: "firebase_core")
+      ],
+      path: "Sources/firebase_core_target"
+    ),
+    .target(
+      name: "firebase_core",
+      path: "packages/firebase_core/firebase_core/ios/firebase_core",
+      exclude: ["Package.swift"],
+      publicHeadersPath: "Sources/firebase_core/include"
     ),
   ]
 )
