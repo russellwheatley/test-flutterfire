1bf1b66b775b9440aabac1a111fb098cd32dd63c
russellwheatley <russellwheatley85@gmail.com>
2024-10-21 17:04:25 +0100

    update path to plugin class

--- Sources/firebase_core_shared/include/FLTFirebasePluginRegistry.h
+++ Sources/firebase_core_shared/include/FLTFirebasePluginRegistry.h
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 #import <Foundation/Foundation.h>
-#import "FLTFirebasePlugin.h"
+#import "include/firebase_core/FLTFirebasePlugin.h"
 
 @interface FLTFirebasePluginRegistry : NSObject
 /**
