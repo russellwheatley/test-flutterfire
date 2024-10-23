f9874136d9e3dca5217149fdfcf3ad32d2d651db
russellwheatley <russellwheatley85@gmail.com>
2024-10-23 13:46:15 +0100

    match package.swift file

--- Package.swift
+++ Package.swift
@@ -8,26 +8,73 @@
 import Foundation
 import PackageDescription
 
-let library_version = "3.6.0"
+enum ConfigurationError: Error {
+  case fileNotFound(String)
+  case parsingError(String)
+  case invalidFormat(String)
+}
 
-// Using this as a wrapper around firebase core, this allows retrieval of it via remote package
-// whilst also preserving firebase core's Package.swift file needed by Flutter
+func loadFirebaseSDKVersion() throws -> String {
+  let firebaseCoreScriptPath = NSString.path(withComponents: [
+    "packages",
+    "firebase_core",
+    "firebase_core",
+    "ios",
+    "firebase_sdk_version.rb",
+  ])
+
+  do {
+    let content = try String(contentsOfFile: firebaseCoreScriptPath, encoding: .utf8)
+    let pattern = #"def firebase_sdk_version!\(\)\n\s+'([^']+)'\nend"#
+    if let regex = try? NSRegularExpression(pattern: pattern, options: []),
+       let match = regex.firstMatch(
+         in: content,
+         range: NSRange(content.startIndex..., in: content)
+       ) {
+      if let versionRange = Range(match.range(at: 1), in: content) {
+        return String(content[versionRange])
+      } else {
+        throw ConfigurationError.invalidFormat("Invalid format in firebase_sdk_version.rb")
+      }
+    } else {
+      throw ConfigurationError.parsingError("No match found in firebase_sdk_version.rb")
+    }
+  } catch {
+    throw ConfigurationError
+      .fileNotFound("Error loading or parsing firebase_sdk_version.rb: \(error)")
+  }
+}
+
+let firebase_sdk_version_string: String
+
+do {
+  firebase_sdk_version_string = try loadFirebaseSDKVersion()
+} catch {
+  fatalError("Failed to load configuration: \(error)")
+}
+
+guard let firebase_sdk_version = Version(firebase_sdk_version_string) else {
+  fatalError("Invalid Firebase SDK version: \(firebase_sdk_version_string)")
+}
+
+// Shared Swift package manager code for firebase core
 let package = Package(
   name: "remote_firebase_core",
   platforms: [
-    .iOS("13.0")
+    .iOS("13.0"),
+    .macOS("10.15"),
   ],
   products: [
-    .library(name: "firebase-core-shared", targets: ["firebase_core_shared"])
+    .library(name: "firebase-core-shared", targets: ["firebase_core_shared"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "11.0.0")
+    .package(url: "https://github.com/firebase/firebase-ios-sdk", exact: firebase_sdk_version),
   ],
   targets: [
     .target(
       name: "firebase_core_shared",
       dependencies: [
-        .product(name: "FirebaseInstallations", package: "firebase-ios-sdk")
+        .product(name: "FirebaseInstallations", package: "firebase-ios-sdk"),
       ],
       path: "Sources/firebase_core_shared",
       publicHeadersPath: "include"
