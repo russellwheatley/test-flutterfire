208011ed31ba4ee1b72d535c74a6c3c4537a35ff
russellwheatley <russellwheatley85@gmail.com>
2024-12-03 16:15:19 +0000

    fix package swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 import PackageDescription
-
+// TODO - this needs updating
+let library_version_string = "3.8.0"
 // Shared Swift package manager code for firebase core
 let package = Package(
   name: "remote_firebase_core",
@@ -16,21 +17,26 @@ let package = Package(
     .macOS("10.15"),
   ],
   products: [
-    .library(name: "firebase-core-shared", targets: ["firebase_core_shared"]),
+    .library(name: "firebase-core-shared", targets: ["firebase_core_shared"])
   ],
   dependencies: [
     // TODO: this is fine for now, but will require a way of retrieving the firebase-ios-sdk
     // likely create a script that runs in preCommit hook of melos
-    .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "11.0.0"),
+    .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "11.0.0")
   ],
   targets: [
     .target(
       name: "firebase_core_shared",
       dependencies: [
-        .product(name: "FirebaseInstallations", package: "firebase-ios-sdk"),
+        .product(name: "FirebaseInstallations", package: "firebase-ios-sdk")
       ],
       path: "Sources/firebase_core_shared",
-      publicHeadersPath: "include"
-    ),
+      publicHeadersPath: "include",
+      cSettings: [
+        .headerSearchPath("include/firebase_core"),
+        .define("LIBRARY_VERSION", to: "\"\(library_version_string)\""),
+        .define("LIBRARY_NAME", to: "\"flutter-fire-core\"")
+      ]
+    )
   ]
 )
